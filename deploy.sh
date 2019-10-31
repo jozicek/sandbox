fe8923ab21c03cdc3d06526e04d1ffaf3055b4bd
jozinko <j.krcmarik@gmail.com>
2019-10-31 20:32:56 +0100

    comments

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+# build project and deploy to payara server
 mvn clean package
 docker cp target/sandbox.war payara:/opt/payara/deployments
 docker cp password.file payara:/opt/payara/passwordFile
-echo 'deploying ...'
+echo 'deploying to payara docker container ...'
 docker exec payara bash -c "asadmin -u admin -W passwordFile --notify deploy --force deployments/sandbox.war"
 
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-
+#starts payara docker container, stop and remove previous if any
 docker stop payara 2> /dev/null
 docker rm payara 2> /dev/null
 docker run -p 8080:8080 --name payara payara/server-full
